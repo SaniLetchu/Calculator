01ffd48ec9e9391ce6f7ba003b73dc64ed3ab83c
Sani Letchu <sani.letchu@gmail.com>
2022-05-17 14:03:41 +0300

    Added some calculator logic

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+
+let screenNumber = "";
+let operatorClicked = 0;
+let operator = null;
+let memoryNumber = "0";
+
+const screenText = document.querySelector('.screentext');
+
+
+function numberPress(number) {
+    if(screenNumber.length < 6) {
+        if(!(number == 0 && screenNumber.length == 0)) {
+            if(operatorClicked == 1) {
+                memoryNumber = screenNumber;
+                screenNumber = "";
+                operatorClicked = 0;
+            }
+            screenNumber += number;
+            screenText.textContent = screenNumber;
+        }
+    }
+}
+
+function operatorSelector(operatorSelection) {
+    operatorClicked = 1;
+    operator = operatorSelection;
+}
+
+function calculate() {
+    let answer = operate(operator, parseInt(memoryNumber), parseInt(screenNumber));
+    operator = null;
+    screenNumber = answer.toString();
+    screenText.textContent = screenNumber;
+}
+
 function add(a, b) {
     return a + b;
 }
